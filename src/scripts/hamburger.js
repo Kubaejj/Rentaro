88794d620edda7c1850c45436e68f7176a290028
Kubaejj <jakub.jezbera.023@pslib.cz>
2025-07-24 22:51:55 +0200

    SOS and header done, faq

--- src/scripts/hamburger.js
+++ src/scripts/hamburger.js
@@ -5,7 +5,20 @@ document.addEventListener("DOMContentLoaded", () => {
             e.preventDefault();
             for (const element of hamelmnts) {
                 element.classList.toggle("active");
-            }  
+            }
         });
     }
+});
+
+const faqItems = document.querySelectorAll('.faq__item');
+
+faqItems.forEach(item => {
+    const question = item.querySelector('.faq__question');
+
+    question.addEventListener('click', () => {
+        faqItems.forEach(i => {
+            if (i !== item) i.classList.remove('active');
+        });
+        item.classList.toggle('active');
+    });
 });
